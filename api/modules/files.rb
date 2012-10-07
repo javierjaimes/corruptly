71ffc9bdcdd253e3e6426f139f5b5df97c7a4842
Javier Jaimes <javierjaimes@gmail.com>
2012-10-07 12:07:38 -0500

    Added files preview

---
+++ api/modules/files.rb
@@ -0,0 +1,17 @@
+require './helpers/auth'
+
+require './models/report'
+
+module Corruptly
+
+  class Files < Application
+
+    get '/:id' do
+      report = Report.where(:id => params[:id]).first()
+      file = report.file
+      
+      [200, {'Content-Type' => file.content_type}, [file.read]]
+    end
+
+  end
+end
