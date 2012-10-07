f13c1f5016c09c4fea812164a698d8a51d22e7fb
Javier Jaimes <javierjaimes@gmail.com>
2012-10-07 11:04:03 -0500

    reports updated

--- modules/reports.rb
+++ modules/reports.rb
@@ -17,9 +17,9 @@ module Corruptly
         :advertising_piece => params['advertising_piece'], 
         :description => params['description'],
         :candidate_id => params['candidate_id'],
-        :file => params[ :file ][ :tempfile ]
       )
-      report.file_name = params[ :file ][ :filename ]
+      report.file = params[ :file ][ :tempfile ] if params[ :file ]
+      report.file_name = params[ :file ][ :filename ] if params[ :file ]
       report.save
       
       # Save attachment
